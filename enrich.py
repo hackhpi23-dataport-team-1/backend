b30e4e8a48d8241e555203ac2940439994046f93
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2023-04-13 23:15:46 +0200

    update filename

--- enrich.py
+++ enrich.py
@@ -2,6 +2,7 @@
 from ipwhois import IPWhois
 from dotenv import load_dotenv
 from datastructures import Vertex, Edge
+import hashlib
 load_dotenv()
 
 def enrich_ip(vertex):
@@ -41,43 +42,44 @@ def enrich_ip(vertex):
 def check_blacklist(input):
     """ Takes a domain or IP address and check if it is blacklisted"""
     try:
-        with open('data/blacklist.txt', 'r') as f:
+        with open('blocklist/ip_blacklist.txt', 'r') as f:
             if input in f.readlines():
                 return True
             return False
     except:
         print('Error: Could not open blacklist.txt')
         return "This is blacklist problem"
-# def enrich_file(vertex):
-#     """
-#     makes a request to the VirusTotal API to get file info and adds
-#     VT_API_KEY as x-apikey to the header
-#     """
-#     file = vertex.attr['file']
-#     # hash file with MD5
-#     hash = hashlib.md5(file.encode('utf-8')).hexdigest()
 
-#     # make request to VirusTotal API
-#     url = 'https://www.virustotal.com/api/v3/files/{hash}'.format(hash=hash)
-#     headers = {
-#         'x-apikey': os.getenv('VT_API_KEY')
-#     }
-#     response = requests.get(url, headers=headers)
+def enrich_file(vertex):
+    """
+    makes a request to the VirusTotal API to get file info and adds
+    VT_API_KEY as x-apikey to the header
+    """
+    file = vertex.attr['file']
+    # hash file with MD5
+    hash = hashlib.md5(file.encode('utf-8')).hexdigest()
 
-#     # extract info and update vertex
-#     response_json = response.json()
-#     result = response_json['data']['attributes']['last_analysis_results']
-#     attrs = {'malicious': False }
-#     # check if in any result dict is a 'result' not none
-#     for key, value in result.items():
-#         if value['result'] != None:
-#             attrs = {
-#                 'malicious': True,
-#                 'malware_detect': key,
-#                 'malware_result': value
-#             }
-#     vertex.update(attrs)
-#     return vertex
+    # make request to VirusTotal API
+    url = 'https://www.virustotal.com/api/v3/files/{hash}'.format(hash=hash)
+    headers = {
+        'x-apikey': os.getenv('VT_API_KEY')
+    }
+    response = requests.get(url, headers=headers)
+
+    # extract info and update vertex
+    response_json = response.json()
+    result = response_json['data']['attributes']['last_analysis_results']
+    attrs = {'malicious': False }
+    # check if in any result dict is a 'result' not none
+    for key, value in result.items():
+        if value['result'] != None:
+            attrs = {
+                'malicious': True,
+                'malware_detect': key,
+                'malware_result': value
+            }
+    vertex.update(attrs)
+    return vertex
 
 
 
