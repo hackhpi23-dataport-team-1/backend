e4711d03a410958fd186d0ac5755eae7032df60a
sonja-soennichsen <sonja.soennichsen@axelspringer.com>
2023-04-13 17:57:48 +0200

    fix import error

--- datastructures.py
+++ datastructures.py
@@ -1,4 +1,4 @@
-from utils import hash_vid
+from utils.hashing import hash_vid
 class Edge:
     def __init__(self, source,  target, kind, attr={}):
         self.source = source
@@ -11,10 +11,10 @@ class Edge:
     
 class Vertex:
     def __init__(self, kind, attr={}):
-        self.id = hash_vid(kind, attr)
         self.kind = kind
         self.score = None
         self.attr = attr
+        self.id = hash_vid(kind, attr)
 
     def add_attribute(self, dict):
         self.attr.update(dict)
