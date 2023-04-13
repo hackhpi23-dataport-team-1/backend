164a37e3c178fb373147594d4f98ffdd369c88dd
Wenzel Pünter <wenzel@puenter.org>
2023-04-13 16:50:03 +0200

    Add simple hashing method for vertex id generation

---
+++ utils/hashing.py
@@ -0,0 +1,14 @@
+# Hashing function to translate a vertex kind + attributes into a consistent
+# hash across different parts of the pipeline.
+import hashlib
+
+def hash_vid(kind, attrs) -> str:
+    featuremap = {
+        'ip': 'value',
+        'as': 'asn',
+        'domain': 'name'
+    }
+
+    data = attrs[featuremap[kind]]
+    result = hashlib.md5(data.encode('utf-8'))
+    return result.hexdigest()
