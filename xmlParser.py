661993105ba4a292edbb703398df94bc739e114f
Wenzel Pünter <wenzel@puenter.org>
2023-04-14 11:47:40 +0200

    Fix indentationg

--- xmlParser.py
+++ xmlParser.py
@@ -8,72 +8,72 @@ def get_md5(hashes):
     return str(hashh)
 
 def parse(path):
-graph = Graph([], [])
+    graph = Graph([], [])
 
-file = open(path, 'r')
-lines = file.readlines()
-for line in lines:
-    root = ET.ElementTree(ET.fromstring(line.replace(" xmlns='http://schemas.microsoft.com/win/2004/08/events/event'", '')))
+    file = open(path, 'r')
+    lines = file.readlines()
+    for line in lines:
+        root = ET.ElementTree(ET.fromstring(line.replace(" xmlns='http://schemas.microsoft.com/win/2004/08/events/event'", '')))
 
-    # Get event ID
-    eid = int(root.find('System').find('EventID').text)
+        # Get event ID
+        eid = int(root.find('System').find('EventID').text)
 
-    # Parse all attributes
-    attrs = {}
-    for attr_element in root.find('EventData').findall('Data'):
-        attrs[attr_element.attrib['Name']] = attr_element.text
+        # Parse all attributes
+        attrs = {}
+        for attr_element in root.find('EventData').findall('Data'):
+            attrs[attr_element.attrib['Name']] = attr_element.text
 
-    vertices = []
-    edges = []
+        vertices = []
+        edges = []
 
-    if eid == 1:
-        from_vtx = Vertex('process', {
-            'ProcessGuid': attrs['ParentProcessGuid'],
-            'Image': attrs['ParentImage']
-        })
-        to_vtx = Vertex('process', {
-            'ProcessGuid': attrs['ProcessGuid'],
-            'Image': attrs['Image']
-        })
-        graph.add_vertex(from_vtx)
-        graph.add_vertex(to_vtx)
-        graph.add_edge(Edge(from_vtx, to_vtx, 'spawn'))
-    elif eid == 2:
-        from_vtx = Vertex('process', {
-            'ProcessGuid': attrs['ProcessGuid'],
-            'Image': attrs['Image']
-        })
-        to_vtx = Vertex('file', {
-            'TargetFilename': attrs['TargetFilename']
-        })
+        if eid == 1:
+            from_vtx = Vertex('process', {
+                'ProcessGuid': attrs['ParentProcessGuid'],
+                'Image': attrs['ParentImage']
+            })
+            to_vtx = Vertex('process', {
+                'ProcessGuid': attrs['ProcessGuid'],
+                'Image': attrs['Image']
+            })
+            graph.add_vertex(from_vtx)
+            graph.add_vertex(to_vtx)
+            graph.add_edge(Edge(from_vtx, to_vtx, 'spawn'))
+        elif eid == 2:
+            from_vtx = Vertex('process', {
+                'ProcessGuid': attrs['ProcessGuid'],
+                'Image': attrs['Image']
+            })
+            to_vtx = Vertex('file', {
+                'TargetFilename': attrs['TargetFilename']
+            })
 
-        graph.add_vertex(from_vtx)
-        graph.add_vertex(to_vtx)
-        graph.add_edge(Edge(from_vtx, to_vtx, 'set-created'))
-    elif eid == 3:
-        from_vtx = Vertex('process', {
-            'ProcessGuid': attrs['ProcessGuid'],
-            'Image': attrs['Image']
-        })
-        to_vtx = Vertex('ip', {
-            'ip': attrs['DestinationIp']
-        })
+            graph.add_vertex(from_vtx)
+            graph.add_vertex(to_vtx)
+            graph.add_edge(Edge(from_vtx, to_vtx, 'set-created'))
+        elif eid == 3:
+            from_vtx = Vertex('process', {
+                'ProcessGuid': attrs['ProcessGuid'],
+                'Image': attrs['Image']
+            })
+            to_vtx = Vertex('ip', {
+                'ip': attrs['DestinationIp']
+            })
 
-        graph.add_vertex(from_vtx)
-        graph.add_vertex(to_vtx)
-        graph.add_edge(Edge(from_vtx, to_vtx, 'connect'))
-    elif eid == 7:
-        from_vtx = Vertex('process', {
-            'ProcessGuid': attrs['ProcessGuid'],
-            'Image': attrs['Image']
-        })
-        to_vtx = Vertex('file', {
-            'TargetFilename': attrs['ImageLoaded'],
-            'HashMD5': get_md5(attrs['Hashes'])
-        })
+            graph.add_vertex(from_vtx)
+            graph.add_vertex(to_vtx)
+            graph.add_edge(Edge(from_vtx, to_vtx, 'connect'))
+        elif eid == 7:
+            from_vtx = Vertex('process', {
+                'ProcessGuid': attrs['ProcessGuid'],
+                'Image': attrs['Image']
+            })
+            to_vtx = Vertex('file', {
+                'TargetFilename': attrs['ImageLoaded'],
+                'HashMD5': get_md5(attrs['Hashes'])
+            })
 
-        graph.add_vertex(from_vtx)
-        graph.add_vertex(to_vtx)
-        graph.add_edge(Edge(from_vtx, to_vtx, 'load'))
+            graph.add_vertex(from_vtx)
+            graph.add_vertex(to_vtx)
+            graph.add_edge(Edge(from_vtx, to_vtx, 'load'))
 
     return path
